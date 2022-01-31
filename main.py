b80afe014261417c5df6b8ced1356b17836e9605
Taylor Loatman <taylor.loatman@icloud.com>
2022-01-30 19:39:31 -0500

    Complete portfolio website.

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, send_file
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sdlkfoinwe4t6ewd2838f398b'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/get-cv')
+def get_cv():
+    path = 'static/client/pdf/resume.pdf'
+    return send_file(path, as_attachment=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
